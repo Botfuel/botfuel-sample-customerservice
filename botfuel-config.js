1f593844687e11d07f8f85e35228b64c2b808beb
daniellepintz <daniellepintz@gmail.com>
2018-04-12 15:53:50 +0200

    fix: changed config and package.json files to reflect latest update

--- botfuel-config.js
+++ botfuel-config.js
@@ -1,6 +1,10 @@
 module.exports = {
-  adapter: 'botfuel',
-  qna: {
-    when: 'before'
+  adapter: { name: 'botfuel' },
+  nlu: {
+  	name: 'botfuel',
+  	qna: {
+    	when: 'before'
+    }
   },
+  logger: 'info'
 };
